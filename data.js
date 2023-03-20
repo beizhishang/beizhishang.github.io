ed047117ade8eae42f41fc6287a69abade9def1c
Austin <xupengyuniphone@icloud.com>
2023-03-20 17:36:33 +0800

    update

---
+++ data.js
@@ -0,0 +1,61 @@
+const data = [
+    {
+        "logo": "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+        "name": "Google",
+        "description": "Search the world's information, including webpages, images, videos and more.",
+        "url": "https://www.google.com",
+        "tag": "search engine"
+    },
+    {
+        "logo": "https://www.facebook.com/images/fb_icon_325x325.png",
+        "name": "Facebook",
+        "description": "Connect with friends and the world around you on Facebook.",
+        "url": "https://www.facebook.com",
+        "tag": "social media"
+    },
+
+    {
+        "logo": "https://www.instagram.com/static/images/ico/favicon-192.png/68d99ba29cc8.png",
+        "name": "Instagram",
+        "description": "Create an account or log in to Instagram - A simple, fun & creative way to capture, edit & share photos, videos & messages with friends & family.",
+        "url": "https://www.instagram.com",
+        "tag": "photo sharing"
+    },
+    {
+        "logo": "https://www.linkedin.com/favicon.ico",
+        "name": "LinkedIn",
+        "description": "675 million+ members | Manageyour professional identity. Build and engage with your professional network. Access knowledge, insights and opportunities.",
+        "url": "https://www.linkedin.com",
+        "tag": "professional networking"
+    },
+    {
+        "logo": "https://www.amazon.com/favicon.ico",
+        "name": "Amazon",
+        "description": "Free delivery on millions of items with Prime. Low prices across earth's biggest selection of books, music, DVDs, electronics, computers, software, apparel & accessories, shoes, jewelry, tools & hardware, housewares, furniture, sporting goods, beauty & personal care, groceries & just about anything else.",
+        "url": "https://www.amazon.com",
+        "tag": "online shopping"
+    },
+    {
+        "logo": "https://www.amazon.com/favicon.ico",
+        "name": "Amazon",
+        "description": "Free delivery on millions of items with Prime. Low prices across earth's biggest selection of books, music, DVDs, electronics, computers, software, apparel & accessories, shoes, jewelry, tools & hardware, housewares, furniture, sporting goods, beauty & personal care, groceries & just about anything else.",
+        "url": "https://www.amazon.com",
+        "tag": "online shopping"
+    },
+    {
+        "logo": "https://www.apple.com/favicon.ico",
+        "name": "Apple",
+        "description": "Discover the innovative world of Apple and shop everything iPhone, iPad, Apple Watch, Mac, and Apple TV, plus explore accessories, entertainment, and expert device support.",
+        "url": "https://www.apple.com",
+        "tag": "technology"
+    },
+    {
+        "logo": "https://www.reddit.com/favicon.ico",
+        "name": "Reddit",
+        "description": "Reddit is a network of communities based on people's interests. Find communities you're interested in, and become part of an online community!",
+        "url": "https://www.reddit.com",
+        "tag": "social news aggregation"
+    }
+
+
+];
