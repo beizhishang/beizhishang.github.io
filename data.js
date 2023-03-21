7b3cf2f8ddc184087d8aeb4cffafeb61095f44fe
Austin <xupengyuniphone@icloud.com>
2023-03-21 14:31:27 +0800

    update

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 const data = [
             {
-                "logo": "https://www.canva.com/favicon.ico",
+                "logo": "https://static.canva.com/web/images/12487a1e0770d29351bd4ce4f87ec8fe.svg",
                 "name": "Canva",
                 "description": "Create beautiful designs with your team. Use Canva's drag-and-drop feature and layouts to design, share and print business cards, logos, presentations and more.",
                 "url": "https://www.canva.com",
@@ -29,7 +29,7 @@ const data = [
                 "tag": "productivity tool"
             },
             {
-                "logo": "https://www.pixabay.com/favicon.ico",
+                "logo": "https://pixabay.com/favicon-32x32.png",
                 "name": "Pixabay",
                 "description": "Over 1.8 million+ high quality stock images and videos shared by our talented community.",
                 "url": "https://www.pixabay.com",
@@ -43,7 +43,7 @@ const data = [
                 "tag": "image stock"
             },
             {
-                "logo": "https://www.pexels.com/favicon.ico",
+                "logo": "https://www.pexels.com/assets/static/images/meta/favicon.ico",
                 "name": "Pexels",
                 "description": "Best free stock photos in one place. Discover and download free images and videos for use in any project.",
                 "url": "https://www.pexels.com",
@@ -90,8 +90,211 @@ const data = [
                 "description": "Happy Hues - Curated colors in context.",
                 "url": "https://www.happyhues.co/",
                 "tag": "color palette"
-            }
+            },
+{
+                "logo": "https://www.tensorflow.org/favicon.ico",
+                "name": "TensorFlow",
+                "description": "An end-to-end open source machine learning platform",
+                "url": "https://www.tensorflow.org/",
+                "tag": "AI"
+            },
+            {
+                "logo": "https://pytorch.org/assets/images/favicon.ico",
+                "name": "PyTorch",
+                "description": "An open source machine learning framework that accelerates the path from research prototyping to production deployment.",
+                "url": "https://pytorch.org/",
+                "tag": "AI"
+            },
+            {
+                "logo": "https://www.kaggle.com/static/images/favicon.ico",
+                "name": "Kaggle",
+                "description": "Kaggle is the world's largest data science community with powerful tools and resources to help you achieve your data science goals.",
+                "url": "https://www.kaggle.com/",
+                "tag": "AI"
+            },
+            {
+                "logo": "https://colab.research.google.com/img/favicon.ico",
+                "name": "Google Colab",
+                "description": "Google Colab is a free cloud service and now it supports free GPU! You can: improve your Python programming language coding skills. develop deep learning applications using popular libraries such as Keras, TensorFlow, PyTorch, and OpenCV.",
+                "url": "https://colab.research.google.com/",
+                "tag": "AI"
+            },
+            {
+                "logo": "https://www.ibm.com/favicon.ico",
+                "name": "IBM Watson",
+                "description": "IBM Watson is a data analysis software that uses natural language processing algorithms to reveal insights from large amounts of unstructured data.",
+                "url": "https://www.ibm.com/watson",
+                "tag": "AI"
+            },
+            {
+                "logo": "https://www.anaconda.com/favicon.ico",
+                "name": "Anaconda",
+                "description": "The World's Most Popular Data Science Platform. Anaconda is the birthplace of Python data science.",
+                "url": "https://www.anaconda.com/",
+                "tag": "AI"
+            },
+            {
+                "logo": "https://www.openai.com/favicon.ico",
+                "name": "OpenAI",
+                "description": "OpenAI is an AI research laboratory consisting of the forerunners of the AI industry. They are dedicated to creating safe AI and ensuring that the benefits of AI are shared with everyone.",
+                "url": "https://www.openai.com/",
+                "tag": "AI"
+            },
+            {
+                "logo": "https://www.shutterstock.com/favicon.ico",
+                "name": "Shutterstock",
+                "description": "Download the best royalty free images from Shutterstock, including photos, vectors, and illustrations. Enjoy straightforward pricing and simple licensing.",
+                "url": "https://www.shutterstock.com/",
+                "tag": "image stock"
+            },
+            {
+                "logo": "https://www.istockphoto.com/favicon.ico",
+                "name": "iStock",
+                "description": "Explore millions of exclusive, royalty-free, stock photos, images, and videos. Find the perfect image for your project, fast. Search now.",
+                "url": "https://www.istockphoto.com/",
+                "tag": "image stock"
+            },
+            {
+                "logo": "https://www.gettyimages.com/favicon.ico",
+                "name": "Getty Images",
+                "description": "Find high-quality stock photos, editorial news pictures, and video footage by searching among the world's leading creative stock image providers.",
+                "url": "https://www.gettyimages.com/",
+                "tag": "image stock"
+            },
+            {
+                "logo": "https://www.freepik.com/favicon.ico",
+                "name": "Freepik",
+                "description": "Find & Download Free Graphic Resources for Graphic Design. 7+ Million Graphic Resources. Free for commercial use ✓ High Quality Images.",
+                "url": "https://www.freepik.com/",
+                "tag": "image stock"
+            },
+            {
+                "logo": "https://www.vectorstock.com/favicon.ico",
+                "name": "VectorStock",
+                "description": "Royalty-free vector images, vector art, graphics, clipart, illustrations and high resolution stock images. Find the vectors you want!",
+                "url": "https://www.vectorstock.com/",
+                "tag": "image stock"
+            },
+            {
+                "logo": "https://www.pngtree.com/favicon.ico",
+                "name": "Pngtree",
+                "description": "Millions of PNG Images, Backgrounds and Vectors for Free Download. Pngtree provides free download of png, png images, backgrounds and vector. Millions of high quality free png images, PSD, AI and EPS Files are available.",
+                "url": "https://www.pngtree.com/",
+                "tag": "image stock"
+            },
+            {
+                "logo": "https://www.ppttemplate.net/wp-content/uploads/2019/01/cropped-ppttemplate-logo-1.png",
+                "name": "PPT Template",
+                "description": "Free PowerPoint Templates and Google Slides Themes",
+                "url": "https://www.ppttemplate.net/",
+                "tag": "PPT template"
+            },
+            {
+                "logo": "https://www.slidescarnival.com/wp-content/uploads/2016/08/favicon512-2-225x225.png",
+                "name": "Slides Carnival",
+                "description": "Free Powerpoint templates and Google Slides themes for your presentations",
+                "url": "https://www.slidescarnival.com/",
+                "tag": "PPT template"
+            },
+            {
+                "logo": "https://www.free-power-point-templates.com/images/favicon.png",
+                "name": "Free PowerPoint Templates",
+                "description": "Download free PowerPoint templates and backgrounds! We have the best collection of professional templates, charts and diagrams.",
+                "url": "https://www.free-power-point-templates.com/",
+                "tag": "PPT template"
+            },
+            {
+                "logo": "https://www.powerpointify.com/wp-content/uploads/2019/06/cropped-Logo-2.png",
+                "name": "PowerPointify",
+                "description": "Free and Premium PowerPoint Templates",
+                "url": "https://www.powerpointify.com/",
+                "tag": "PPT template"
+            },
+            {
+                "logo": "https://www.showeet.com/wp-content/uploads/2017/09/cropped-showeet-logo-1.png",
+                "name": "Showeet",
+                "description": "Free PowerPoint Templates and Google Slides Themes",
+                "url": "https://www.showeet.com/",
+                "tag": "PPT template"
+            },
+            {
+                "logo": "https://www.slidemodel.com/wp-content/uploads/2016/05/cropped-SlideModel-Logo-1.png",
+                "name": "SlideModel",
+                "description": "Professional PowerPoint Templates & Slides",
+                "url": "https://www.slidemodel.com/",
+                "tag": "PPT template"
+            },
+            {
+                "logo": "https://www.presentationgo.com/wp-content/uploads/2018/03/cropped-presentationgo-logo-1.png",
+                "name": "PresentationGO",
+                "description": "Free PowerPoint Templates and Google Slides Themes",
+                "url": "https://www.presentationgo.com/",
+                "tag": "PPT template"
+            },
+            {
+                "logo": "https://www.slidescarnival.com/wp-content/uploads/2017/09/favicon.png",
+                "name": "Slides Carnival",
+                "description": "Free Powerpoint templates and Google Slides themes for your presentations",
+                "url": "https://www.slidescarnival.com/",
+                "tag": "PPT template"
+            },
+            {
+                "logo": "https://www.51pptmoban.com/favicon.ico",
+                "name": "51PPT模板网",
+                "description": "提供免费PPT模板下载，PPT素材下载，PPT图表下载，PPT背景图片下载，PPT教程下载，PPT软件下载，PPT字体下载，PPT配色方案下载，PPT插件下载，PPT教程视频下载等资源下载。",
+                "url": "https://www.51pptmoban.com/",
+                "tag": "PPT template"
+            },
+            {
+                "logo": "https://www.pptstore.net/favicon.ico",
+                "name": "PPTSTORE",
+                "description": "PPTSTORE是一个专业的PPT模板下载网站，提供各类高质量的PPT模板下载，包括商务、教育、科技、艺术等多种类型，满足您的各种需求。",
+                "url": "https://www.pptstore.net/",
+                "tag": "PPT template"
+            },
+            {
+                "logo": "https://www.pptplus.cn/favicon.ico",
+                "name": "PPTPlus",
+                "description": "PPTPlus是一个专业的PPT模板下载网站，提供各类高质量的PPT模板下载，包括商务、教育、科技、艺术等多种类型，满足您的各种需求。",
+                "url": "https://www.pptplus.cn/",
+                "tag": "PPT template"
+            },
+            {
+                "logo": "https://www.pptbz.com/d/file/2014/12/03/4d7d1d9d5d7d7f7f7d7d7d7f7f7d7d7d.jpg",
+                "name": "PPT宝藏",
+                "description": "提供免费PPT模板下载，PPT素材下载，PPT图表下载，PPT背景图片下载，PPT教程下载，PPT软件下载，PPT字体下载，PPT配色方案下载，PPT插件下载，PPT教程视频下载等资源下载。",
+                "url": "https://www.pptbz.com/",
+                "tag": "PPT template"
+            },
+            {
+                "logo": "https://www.51ppt.com.cn/favicon.ico",
+                "name": "51PPT",
+                "description": "51PPT模板网是一个专业的PPT模板下载网站，提供各类高质量的PPT模板下载，包括商务、教育、科技、艺术等多种类型，满足您的各种需求。",
+                "url": "https://www.51ppt.com.cn/",
+                "tag": "PPT template"
+            },
+            {
+                "logo": "https://www.pptfans.cn/favicon.ico",
+                "name": "PPTFans",
+                "description": "PPTFans是一个专业的PPT模板下载网站，提供各类高质量的PPT模板下载，包括商务、教育、科技、艺术等多种类型，满足您的各种需求。",
+                "url": "https://www.pptfans.cn/",
+                "tag": "PPT template"
+            },
+            {
+                "logo": "https://www.pptbz.com/d/file/2014/12/03/4d7d1d9d5d7d7f7f7d7d7d7f7f7d7d7d.jpg",
+                "name": "PPT之家",
+                "description": "PPT之家是一个专业的PPT模板下载网站，提供各类高质量的PPT模板下载，包括商务、教育、科技、艺术等多种类型，满足您的各种需求。",
+                "url": "https://www.pptbz.com/",
+                "tag": "PPT template"
+            },
+            {
+                "logo": "https://www.pptok.com/favicon.ico",
+                "name": "PPTOK",
+                "description": "PPTOK是一个专业的PPT模板下载网站，提供各类高质量的PPT模板下载，包括商务、教育、科技、艺术等多种类型，满足您的各种需求。",
+                "url": "https://www.pptok.com/",
+                "tag": "PPT template"
+            },
+            
 
+];
 
-
-        ];
