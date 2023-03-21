4ee04e211baf49ff71d86a2bcdb06322b57a264d
Austin <xupengyuniphone@icloud.com>
2023-03-21 09:36:00 +0800

    update

--- data.js
+++ data.js
@@ -1,61 +1,97 @@
 const data = [
-    {
-        "logo": "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
-        "name": "Google",
-        "description": "Search the world's information, including webpages, images, videos and more.",
-        "url": "https://www.google.com",
-        "tag": "search engine"
-    },
-    {
-        "logo": "https://www.facebook.com/images/fb_icon_325x325.png",
-        "name": "Facebook",
-        "description": "Connect with friends and the world around you on Facebook.",
-        "url": "https://www.facebook.com",
-        "tag": "social media"
-    },
+            {
+                "logo": "https://www.canva.com/favicon.ico",
+                "name": "Canva",
+                "description": "Create beautiful designs with your team. Use Canva's drag-and-drop feature and layouts to design, share and print business cards, logos, presentations and more.",
+                "url": "https://www.canva.com",
+                "tag": "design tool"
+            },
+            {
+                "logo": "https://www.notion.so/images/favicon.ico",
+                "name": "Notion",
+                "description": "A productivity tool for notes, tasks, wikis, and databases.",
+                "url": "https://www.notion.so",
+                "tag": "productivity tool"
+            },
 
-    {
-        "logo": "https://www.instagram.com/static/images/ico/favicon-192.png/68d99ba29cc8.png",
-        "name": "Instagram",
-        "description": "Create an account or log in to Instagram - A simple, fun & creative way to capture, edit & share photos, videos & messages with friends & family.",
-        "url": "https://www.instagram.com",
-        "tag": "photo sharing"
-    },
-    {
-        "logo": "https://www.linkedin.com/favicon.ico",
-        "name": "LinkedIn",
-        "description": "675 million+ members | Manageyour professional identity. Build and engage with your professional network. Access knowledge, insights and opportunities.",
-        "url": "https://www.linkedin.com",
-        "tag": "professional networking"
-    },
-    {
-        "logo": "https://www.amazon.com/favicon.ico",
-        "name": "Amazon",
-        "description": "Free delivery on millions of items with Prime. Low prices across earth's biggest selection of books, music, DVDs, electronics, computers, software, apparel & accessories, shoes, jewelry, tools & hardware, housewares, furniture, sporting goods, beauty & personal care, groceries & just about anything else.",
-        "url": "https://www.amazon.com",
-        "tag": "online shopping"
-    },
-    {
-        "logo": "https://www.amazon.com/favicon.ico",
-        "name": "Amazon",
-        "description": "Free delivery on millions of items with Prime. Low prices across earth's biggest selection of books, music, DVDs, electronics, computers, software, apparel & accessories, shoes, jewelry, tools & hardware, housewares, furniture, sporting goods, beauty & personal care, groceries & just about anything else.",
-        "url": "https://www.amazon.com",
-        "tag": "online shopping"
-    },
-    {
-        "logo": "https://www.apple.com/favicon.ico",
-        "name": "Apple",
-        "description": "Discover the innovative world of Apple and shop everything iPhone, iPad, Apple Watch, Mac, and Apple TV, plus explore accessories, entertainment, and expert device support.",
-        "url": "https://www.apple.com",
-        "tag": "technology"
-    },
-    {
-        "logo": "https://www.reddit.com/favicon.ico",
-        "name": "Reddit",
-        "description": "Reddit is a network of communities based on people's interests. Find communities you're interested in, and become part of an online community!",
-        "url": "https://www.reddit.com",
-        "tag": "social news aggregation"
-    }
+            {
+                "logo": "https://www.trello.com/favicon.ico",
+                "name": "Trello",
+                "description": "Infinitely flexible. Incredibly easy to use. Great mobile apps. It's free. Trello keeps track of everything, from the big picture to the minute details.",
+                "url": "https://www.trello.com",
+                "tag": "productivity tool"
+            },
+            {
+                "logo": "https://www.asana.com/favicon.ico",
+                "name": "Asana",
+                "description": "Work anytime, anywhere with Asana. Keep remote and distributed teams, and your entire organization, focused on their goals, projects, and tasks with Asana.",
+                "url": "https://www.asana.com",
+                "tag": "productivity tool"
+            },
+            {
+                "logo": "https://www.pixabay.com/favicon.ico",
+                "name": "Pixabay",
+                "description": "Over 1.8 million+ high quality stock images and videos shared by our talented community.",
+                "url": "https://www.pixabay.com",
+                "tag": "image stock"
+            },
+            {
+                "logo": "https://www.unsplash.com/favicon.ico",
+                "name": "Unsplash",
+                "description": "Beautiful, free images and photos that you can download and use for any project. Better than any royalty free or stock photos.",
+                "url": "https://www.unsplash.com",
+                "tag": "image stock"
+            },
+            {
+                "logo": "https://www.pexels.com/favicon.ico",
+                "name": "Pexels",
+                "description": "Best free stock photos in one place. Discover and download free images and videos for use in any project.",
+                "url": "https://www.pexels.com",
+                "tag": "image stock"
+            },
+            {
+                "logo": "https://colorhunt.co/img/color-hunt-logo.png",
+                "name": "Color Hunt",
+                "description": "Color Hunt is a free and open platform for color inspiration with thousands of trendy hand-picked color palettes",
+                "url": "https://colorhunt.co/",
+                "tag": "color palette"
+            },
+            {
+                "logo": "https://coolors.co/assets/img/coolors_logo.svg",
+                "name": "Coolors",
+                "description": "The super fast color schemes generator!",
+                "url": "https://coolors.co/",
+                "tag": "color palette"
+            },
+            {
+                "logo": "https://color.adobe.com/create/favicon.ico",
+                "name": "Adobe Color",
+                "description": "Create color palettes with the color wheel, hex or image. Browse thousands of color combinations for usage in Photoshop, Illustrator etc.",
+                "url": "https://color.adobe.com/create/color-wheel",
+                "tag": "color palette"
+            },
+            {
+                "logo": "https://www.materialpalette.com/favicon.ico",
+                "name": "Material Palette",
+                "description": "Choose your favorite colors and get your Material Design palette generated and downloadable.",
+                "url": "https://www.materialpalette.com/",
+                "tag": "color palette"
+            },
+            {
+                "logo": "https://www.colourlovers.com/favicon.ico",
+                "name": "Colourlovers",
+                "description": "Color Trends + Palettes :: COLOURlovers",
+                "url": "https://www.colourlovers.com/",
+                "tag": "color palette"
+            },
+            {
+                "logo": "https://www.happyhues.co/favicon.ico",
+                "name": "Happy Hues",
+                "description": "Happy Hues - Curated colors in context.",
+                "url": "https://www.happyhues.co/",
+                "tag": "color palette"
+            }
 
 
-];
+
+        ];
